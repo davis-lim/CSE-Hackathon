67f0f02b1e7e53095e9e1bee6eb29ed07d72704b
Davis <davislimzh1@gmail.com>
2024-06-30 03:32:07 +1000

    Added hive

--- my-app/frontend/src/pages/Dashboard.jsx
+++ my-app/frontend/src/pages/Dashboard.jsx
@@ -4,34 +4,63 @@ import { useNavigate, Navigate } from 'react-router-dom';
 function MyComponent() {
     const navigate = useNavigate();
     const navigateToRego = () => {
-        navigate('/');
+        navigate('/hive');
     }
-  return (
+    return (
     <>
       <div className="div">
         <div className="div-2">
-          <div className="div-3">
-            <div className="div-4">Home</div>
-          </div>
+            <div className="div-3">
+                <div className="div-4">Home</div>
+            </div>
         </div>
         <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&"
-          className="img"
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/743be299490fdfe6da31a40e70b598e4ffbf6467fd27275ee969b22cab6ba175?apiKey=5880849d19014bf7921644dd08a954e6&"
+            className="img"
         />
         <img
-          loading="lazy"
-          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&"
-          className="hive"
-          onClick={navigateToRego}
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e753a817da62a0d300efb6846e775858093839f0b7cc8881c2e62bf694a8fed3?apiKey=5880849d19014bf7921644dd08a954e6&"
+            className="hive"
+            onClick={navigateToRego}
+        />
+        <img
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/c95a91f10e4d5c63421433818f5a0a10c5dc6250efe40a992ad1d8a5b561f83e?apiKey=5880849d19014bf7921644dd08a954e6&"
+        className="edit"
         />
         <div className="div-6">
-          <div className="div-7">Daily Tasks </div>
+            <div className="div-7">Daily Tasks </div>
         </div>
-        <div className="div-9">Go for a walk in the park</div> 
-        <div className="div-10" />
-        <div className="div-11" />
-      </div>
+        <div className="div-9">Go for a run</div> 
+        <div className="div-10">Write down goals for today</div> 
+        <div className="div-11">Meditate for 20mins</div>
+        <div className="div-12">
+            <div className="navbarpics">
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/66cdcae9ee8ea68fa549a3a7460b8b98387941dc866f647e5172ea79ca76b07d?apiKey=5880849d19014bf7921644dd08a954e6&"
+                className="memz"
+                />
+                <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/9bec2f47ce4baf6a47d76a6b24172e0131237f88be2127450ade0827e8d93ae8?apiKey=5880849d19014bf7921644dd08a954e6&"
+                    className="home"
+                />
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/a6db6a22ec5420922fd49704bf84c2573cfcc8d8aa5f299541b097d3b2058995?apiKey=5880849d19014bf7921644dd08a954e6&"
+                className="friends"
+                />
+            </div>
+            <div className="navbarheadings">
+                <span>Memories</span>
+                <span>Home</span>
+                <span className="friens">Friends</span>
+            </div>
+        </div>
+        </div>
       <style jsx>{`
         .div {
           background-color: #fff5bb;
@@ -46,27 +75,80 @@ function MyComponent() {
             margin: 0 auto;
             padding: 80px 35px;
         }
+        .navbarheadings {
+            display: flex;
+
+        }
         .hive {
             aspect-ratio: 1;
-            position: absolute;
+            position: fixed;
             object-fit: auto;
             object-position: center;
-            width: 2.5%;
-            margin-top: 50px;
-            margin-left: 33px;
+            width: 60px;
+            margin-top: 59px;
+            margin-left: 8px;
+        }
+        .edit {
+            aspect-ratio: 1;
+            position: fixed;
+            object-fit: auto;
+            object-position: center;
+            width: 60px;
+            margin-top: 58px;
+            margin-left: 360px;
         }
         .div-2 {
-          background-color: #ffa800;
-          align-self: stretch;
-          display: flex;
-          width: 380px;
-          flex-direction: column;
-          align-items: center;
-          font-size: 26px;
-          white-space: nowrap;
-          margin-top: -80px;
-          margin-left: -35px;
-          padding: 40px 60px 18px;
+            background-color: #ffa800;
+            align-self: stretch;
+            display: flex;
+            width: 380px;
+            flex-direction: column;
+            align-items: center;
+            font-size: 26px;
+            white-space: nowrap;
+            margin-top: -80px;
+            margin-left: -35px;
+            padding: 40px 60px 18px;
+        }
+        .div-12 {
+            background-color: #ffa800;
+            align-self: stretch;
+            position: fixed;
+            display: flex;
+            width: 380px;
+            flex-direction: column;
+            font-size: 26px;
+            white-space: nowrap;
+            margin-top: 800px;
+            margin-left: -35px;
+            padding: 20px 60px 60px;
+        }
+        .navbarpics{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 20px;
+        }
+        .navbarheadings{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            margin-left: -10px;
+            font-size: 0.7em;
+        }
+        .friens{
+            margin-right: -15px;
+            margin-left: 18px
+        }
+        .memz {
+            width: 35px;
+            
+        }
+        .home {
+            width: 35px;
+        }
+        .friends {
+            width: 35px;
         }
         .div-3 {
           display: flex;
@@ -88,8 +170,8 @@ function MyComponent() {
           aspect-ratio: 1.16;
           object-fit: auto;
           object-position: center;
-          width: 430px;
-          align-self: stretch;
+          margin-left: -35px;
+          width: 500px;
         }
         .div-6 {
           border-radius: 25px;
@@ -125,7 +207,9 @@ function MyComponent() {
           margin-top: 30px;
           font-weight: bold;
           font-size: 1.2em;
-          align-text: center;
+          align-items: center;
+          display: flex;
+          justify-content: center;
         }
         .div-10 {
           border-radius: 25px;
@@ -135,6 +219,11 @@ function MyComponent() {
           width: 100%;;
           max-width: 100%;
           height: 66px;
+          font-weight: bold;
+          font-size: 1.2em;
+          align-items: center;
+          display: flex;
+          justify-content: center;
         }
         .div-11 {
           border-radius: 25px;
@@ -144,6 +233,12 @@ function MyComponent() {
           width: 100%;;
           max-width: 100%;
           height: 66px;
+          font-weight: bold;
+          font-size: 1.2em;
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          margin-bottom: 30px;
         }
       `}</style>
     </>
